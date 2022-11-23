30043033f9e219b2e89c460ad930a095621c311d
Franco  Guinzburg <fguinzburg0@gmail.com>
2022-11-23 15:37:11 -0300

    pintamos toda la casa

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+import NavBar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+import ItemListContainer from './ItemListContainer'
+import NavBarra from './NavBarra'
+
+function NavBar() {
+  return (
+    <>
+    <NavBarra/>
+    <ItemListContainer greeting='y sin dejar caer una sola gota de pintura que no seaQUEESESOOOOOO'/>
+    </>
+  )
+}
+
+export default NavBar
